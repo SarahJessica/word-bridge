8552427d3c63258012b8365953864857a96b3ebd
Jessica Leivers <sarahjessicaleivers@gmail.com>
2017-09-20 23:40:16 +0100

    adds new end of game view controller

---
+++ Word Bridge/Word Bridge/Controller/SuccessViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SuccessViewController.swift
+//  Word Bridge
+//
+//  Created by Jess Leivers on 20/09/2017.
+//  Copyright © 2017 Sarah Jessica London. All rights reserved.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
